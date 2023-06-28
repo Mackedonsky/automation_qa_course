5d0b180bfd9cd77b3ec55dd487540cb6bbb25865
Mackedonsky <alexmackqa@gmail.com>
2023-06-27 23:29:04 -0700

    added class: TestAlertsPage
    added tests:
    - test_see_alert
    - test_alert_appear_5_sec
    - test_confirm_alert
    - test_prompt_alert

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -5,3 +5,15 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON = (By.CSS_SELECTOR, 'button[id = "tabButton"]')
     TITLE_NEW_TAB = (By.CSS_SELECTOR, 'h1[id = "sampleHeading"]')
     NEW_WINDOW_BUTTON = (By.CSS_SELECTOR, 'button[id = "windowButton"]')
+
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id = "alertButton"]')
+    APPEAR_ALERT_5_SEC_BUTTON = (By.CSS_SELECTOR, 'button[id = "timerAlertButton"]')
+    CONFIRM_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id = "confirmButton"]')
+    CONFIRM_RESULT = (By.CSS_SELECTOR, 'span[id = "confirmResult"]')
+    PROMPT_RESULT = (By.CSS_SELECTOR, 'span[id = "promptResult"]')
+    PROMPT_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id = "promtButton"]')
+
+
+
