9890aafeb3148ac9c5346e02b4a52d9091a6edf8
Mackedonsky <alexmackqa@gmail.com>
2023-07-26 12:58:03 -0700

    added class: TestAutoCompletePage added tests: - test_fill_multi_autocomplete - test_remove_value_from_multi - test_fill_single_autocomplete

--- generator/generator.py
+++ generator/generator.py
@@ -1,5 +1,5 @@
 import random
-from data.data import Person
+from data.data import Person, Color
 from faker import Faker
 
 faker_ru = Faker('ru_RU')
@@ -26,3 +26,9 @@ def generated_file():
     file.write(f'Hello world!{random.randint(0, 999)}')
     file.close()
     return file.name, path
+
+
+def generated_color():
+    yield Color(
+        color_name=["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
+    )
